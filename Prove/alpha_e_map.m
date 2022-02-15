8340f923b15e18bc7d69d7b51fb1856baa7eb2cf
EmanueleSticchi <emanuele.sticchi@live.it>
2022-02-15 13:10:38 +0100

    prova di alpha_map

--- Prove/alpha_e_map.m
+++ Prove/alpha_e_map.m
@@ -7,14 +7,14 @@ V_inf   = linspace(0.1,convvel(293,'km/h','m/s'),dim_vel);
 Chi     = convang(5,'deg','rad');
 f       = 3;
 W       = 75278;
-theta_t = convang(-8,'deg','rad');
+rotore1.theta_t = convang(-8,'deg','rad');
 rotore1.h     = 0;
 % properties
 rotore1 = rotore1.r(linspace(0.1,1,100));
 rotore1.R     = 7.6;
 rotore1.N     = 3;
 rotore1.c     = linspace(0.4,0.4,rotore1.n_r);
-rotore1.theta = pi/180*linspace(13.3,9,rotore1.n_r);
+% rotore1.theta = pi/180*linspace(13.3,9,rotore1.n_r);
 % obj.I = convmass(obj.Ixx,'slug','kg');
 % obj.I = convlength(convlength(obj.Ixx,'ft','m'),'ft','m');
 % function recall
@@ -22,7 +22,18 @@ rotore1 = rotore1.ambient();
 
 rotore1 = rotore1.mass_prop('G',8);
 rotore1 = rotore1.rot_vel('omega',1,1);
-rotore1 = rotore1.BEMT_articulated(V_inf,Chi,f,W,theta_t);
+rotore1 = rotore1.BEMT_articulated('T',W,V_inf,Chi,f);
+
+alphamap(rotore1,'Plot',{rotore1.Analisi_articulated{1,1};[0.1, 0.2, 0.3]})
+
+
+alphamap(rotore1,'Solve',{'T',W,V_inf([10,20,30]),Chi,f,BEMTset_rotor()})
+
+
+
+
+
+
 % W1 = W*1.3;
 % rotore1 = rotore1.BEMT_articulated(V_inf,Chi,f,W1,theta_t);
 % W2 = W*1.5;
@@ -68,51 +79,51 @@ rotore1 = rotore1.BEMT_articulated(V_inf,Chi,f,W,theta_t);
 % end
 
 
-
-
-
-%% Alpha_e map 
-s = rotore1.Analisi_articulated{1,1};
-idxV = 50;
-figure
-alpha_e=s.alpha_e(:,:,idxV)*180/pi;
-% alpha_e(alpha_e < -10) = -10;
-% contour(rotore1.r_bar,s.options.Psi,alpha_e')
-
-% Create polar data
-[r,psi] = meshgrid(rotore1.r_bar,s.options.Psi);
-% Convert to Cartesian
-x = r.*cos(psi);
-y = r.*sin(psi);
-
-% define polar axes
-h = polar(x,y);
-hold on;
-polar(s.options.Psi,rotore1.r_bar(1)*ones(length(s.options.Psi),1),'k')
-polar(s.options.Psi,rotore1.r_bar(end)*ones(length(s.options.Psi),1),'k')
-% contourf(x,y,alpha_e');
-pc= pcolor(x,y,alpha_e');
-contour(x,y,alpha_e','k','ShowText','on');
-
-shading interp
-% colormap 'hsv'
-cbar=colorbar(gca);
-cbar.Label.String = '\alpha_e';
-cbar.Label.FontSize= 16;
-% cbar.Limits = [-10 10];
-
-
-
-% Hide the POLAR function data and leave annotations
-set(h,'Visible','off')
-% Turn off axes and set square aspect ratio
-axis off
-axis image
-view([90 90])
-title(['\alpha_{e_{max}} = ',num2str(max(alpha_e,[],'all')),' deg'])
-
-m=max(abs(alpha_e),[],'all');
-[r,c] = find(abs(alpha_e)==m)
+% 
+% 
+% 
+% %% Alpha_e map 
+% s = rotore1.Analisi_articulated{1,1};
+% idxV = 50;
+% figure
+% alpha_e=s.alpha_e(:,:,idxV)*180/pi;
+% % alpha_e(alpha_e < -10) = -10;
+% % contour(rotore1.r_bar,s.options.Psi,alpha_e')
+% 
+% % Create polar data
+% [r,psi] = meshgrid(rotore1.r_bar,s.options.Psi);
+% % Convert to Cartesian
+% x = r.*cos(psi);
+% y = r.*sin(psi);
+% 
+% % define polar axes
+% h = polar(x,y);
+% hold on;
+% polar(s.options.Psi,rotore1.r_bar(1)*ones(length(s.options.Psi),1),'k')
+% polar(s.options.Psi,rotore1.r_bar(end)*ones(length(s.options.Psi),1),'k')
+% % contourf(x,y,alpha_e');
+% pc= pcolor(x,y,alpha_e');
+% contour(x,y,alpha_e','k','ShowText','on');
+% 
+% shading interp
+% % colormap 'hsv'
+% cbar=colorbar(gca);
+% cbar.Label.String = '\alpha_e';
+% cbar.Label.FontSize= 16;
+% % cbar.Limits = [-10 10];
+% 
+% 
+% 
+% % Hide the POLAR function data and leave annotations
+% set(h,'Visible','off')
+% % Turn off axes and set square aspect ratio
+% axis off
+% axis image
+% view([90 90])
+% title(['\alpha_{e_{max}} = ',num2str(max(alpha_e,[],'all')),' deg'])
+% 
+% m=max(abs(alpha_e),[],'all');
+% [r,c] = find(abs(alpha_e)==m)
 
 
 %% Graphics
