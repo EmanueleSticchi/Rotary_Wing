94ebcd0bb1d6db6e4a1d9e216ac89b1fff8a7682
EmanueleSticchi <emanuele.sticchi@live.it>
2022-02-15 13:09:12 +0100

    aggiunta k in Pc0

--- function/BEMTset_rotor.m
+++ function/BEMTset_rotor.m
@@ -3,7 +3,10 @@ classdef BEMTset_rotor
         % flag for Prandtl correction
         P_correction  {mustBeMember(P_correction,{'on','off',})} = 'off';
         % integrals upper bound for the Pradtl Correction 
-        B       (1,1) {mustBeInRange(B,0,1,'exclude-lower')}     = 1
+        B       (1,1) {mustBeInRange(B,0,1,'exclude-lower')}     = 1;
+        % Fattore di scorrimento per il calcolo della potenza parassita
+        % Pc0 = sigma*Cd_mean/8*(1+k*mu^2)
+        k                                                        =3;
         % stop iteration when res < toll
         toll                                                     = 1e-6;
         % Psi domain for the alpha_e map
