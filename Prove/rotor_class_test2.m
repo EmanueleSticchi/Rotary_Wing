fd6e2a5bbb9b660cf6ff6ffd7be58ca9954052db
EmanueleSticchi <emanuele.sticchi@live.it>
2022-02-15 13:10:59 +0100

    update in test2

--- Prove/rotor_class_test2.m
+++ Prove/rotor_class_test2.m
@@ -7,14 +7,14 @@ V_inf   = linspace(0.1,convvel(293,'km/h','m/s'),dim_vel);
 Chi     = convang(5,'deg','rad');
 f       = 3;
 W       = 75278;
-theta_t = convang(-8,'deg','rad');
+rotore1.theta_t = convang(-8,'deg','rad');
 rotore1.h     = 0;
 % properties
 rotore1 = rotore1.r(linspace(0.1,1,20));
 rotore1.R     = 7.6;
 rotore1.N     = 3;
 rotore1.c     = linspace(0.4,0.4,rotore1.n_r);
-rotore1.theta = pi/180*linspace(13.3,9,rotore1.n_r);
+% rotore1.theta = pi/180*linspace(13.3,9,rotore1.n_r);
 % obj.I = convmass(obj.Ixx,'slug','kg');
 % obj.I = convlength(convlength(obj.Ixx,'ft','m'),'ft','m');
 % function recall
@@ -22,11 +22,11 @@ rotore1 = rotore1.ambient();
 
 rotore1 = rotore1.mass_prop('G',8);
 rotore1 = rotore1.rot_vel('omega',1,1);
-rotore1 = rotore1.BEMT_articulated(V_inf,Chi,f,W,theta_t);
+rotore1 = rotore1.BEMT_articulated('T',W,V_inf,Chi,f);
 W1 = W*1.3;
-rotore1 = rotore1.BEMT_articulated(V_inf,Chi,f,W1,theta_t);
+rotore1 = rotore1.BEMT_articulated('T',W1,V_inf,Chi,f);
 W2 = W*1.5;
-rotore1 = rotore1.BEMT_articulated(V_inf,Chi,f,W2,theta_t);
+rotore1 = rotore1.BEMT_articulated('T',W2,V_inf,Chi,f);
 
 
 %% Graphics
