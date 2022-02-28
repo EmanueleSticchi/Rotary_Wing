b521305012aeeab16192ded28151f56b5d936f8f
EmanueleSticchi <emanuele.sticchi@live.it>
2022-02-28 20:12:19 +0100

    Analisi Volo traslato Rotore principale
    problema su Theta0 che supera il valore limite di 19 deg

---
+++ Black Hawk/Volo_Traslato.m
@@ -0,0 +1,68 @@
+%%  Analisi Rotore Principale in volo tralato
+clc; clear; close all
+global aero
+%% Data -------------------------------------------------------------------
+rotore   = Rotor();
+% aerodynamics
+load('polari HH_02\polari\Aero_HH02_Re1250.mat')
+rotore.Cl_alpha = aero.Cl_a_mode;
+rotore.Cl       = @(alpha) CL_(alpha);
+rotore.Cd       = @(alpha) CD_(alpha);
+
+% working conditions
+rotore   = rotore.rot_vel('omega',convvel(726,'ft/s','m/s')/14.63);
+rotore.h = 0;                        % Assume density air = 1.23 Kg/m^3
+rotore   = rotore.ambient();         % compute ambient conditions
+V_inf    = linspace(0.1,convvel(200,'km/h','m/s'));
+Chi      = convang(0.001,'deg','rad');
+f        = 2;
+W        = convforce(3000,'lbf','N');
+
+% geometry
+rotore   = rotore.r(linspace(0.1,1,20));      % dominio radiale [\]
+rotore.R = 14.63;                             % Raggio rotore   [m]
+rotore.N = 4;                                 % numero di pale  [\]
+rotore.c = linspace(0.53,0.53,rotore.n_r);    % corde           [m]
+I_MR     = 3800;
+I_MR     = convmass(I_MR,'slug','kg');
+I_MR     = convlength(convlength(I_MR,'ft','m'),'ft','m');
+rotore   = rotore.mass_prop('I',I_MR);        % Mom. di inerzia [Kg*m^2]
+rotore.theta_t = convang(-9,'deg','rad');     % theta twist     [deg]
+
+
+%%  Analisys --------------------------------------------------------------
+rotore = rotore.BEMT_articulated('T',W,V_inf,Chi,f,BEMTset_rotor());
+
+%% Graphics ---------------------------------------------------------------
+s = rotore.Analisi_articulated{1,1};
+figure;
+plot(s.mu,s.Pc_Vec,'-k');
+hold on
+
+figure;
+plot(s.mu,s.Tc,'-k');
+hold on
+
+figure;
+plot(s.mu,s.theta0*180/pi,'-k');
+hold on
+
+figure;
+plot(s.mu,s.alpha_TPP_Vec*180/pi,'-k');
+hold on
+
+figure;
+plot(s.mu,s.lam_Vec,'-k');
+hold on
+plot(s.mu,s.lam_i,':k');
+plot(s.mu,s.lam_c,'.-k');
+
+figure;
+plot(s.mu,s.beta0_Vec*180/pi,'-k');
+hold on
+plot(s.mu,s.beta1c_Vec*180/pi,':k');
+plot(s.mu,s.beta1s_Vec*180/pi,'.-k');
+
+
+
+
