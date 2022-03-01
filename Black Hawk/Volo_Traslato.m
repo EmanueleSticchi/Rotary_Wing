30e8a4f28c270b71b8711ba25d9e60f658736d9b
EmanueleSticchi <emanuele.sticchi@live.it>
2022-03-01 17:31:11 +0100

    Correzioni
    Sembra corretto per basse V_inf ma i coefficienti di flappeggio sembrano un po' strani

--- Black Hawk/Volo_Traslato.m
+++ Black Hawk/Volo_Traslato.m
@@ -5,12 +5,12 @@ global aero
 rotore   = Rotor();
 % aerodynamics
 load('polari HH_02\polari\Aero_HH02_Re1250.mat')
-rotore.Cl_alpha = aero.Cl_a_mode;
+rotore.Cl_alpha = aero.Cl_a_mode*180/pi;
 rotore.Cl       = @(alpha) CL_(alpha);
 rotore.Cd       = @(alpha) CD_(alpha);
 
 % working conditions
-rotore   = rotore.rot_vel('omega',convvel(726,'ft/s','m/s')/14.63);
+rotore   = rotore.rot_vel('omega',726/24); % [rad/s]
 rotore.h = 0;                        % Assume density air = 1.23 Kg/m^3
 rotore   = rotore.ambient();         % compute ambient conditions
 V_inf    = linspace(0.1,convvel(200,'km/h','m/s'));
@@ -20,13 +20,14 @@ W        = convforce(3000,'lbf','N');
 
 % geometry
 rotore   = rotore.r(linspace(0.1,1,20));      % dominio radiale [\]
-rotore.R = 14.63;                             % Raggio rotore   [m]
+rotore.R = convlength(24,'ft','m');           % Raggio rotore   [m]
 rotore.N = 4;                                 % numero di pale  [\]
 rotore.c = linspace(0.53,0.53,rotore.n_r);    % corde           [m]
 I_MR     = 3800;
 I_MR     = convmass(I_MR,'slug','kg');
 I_MR     = convlength(convlength(I_MR,'ft','m'),'ft','m');
 rotore   = rotore.mass_prop('I',I_MR);        % Mom. di inerzia [Kg*m^2]
+% rotore   = rotore.mass_prop('G',7);
 rotore.theta_t = convang(-9,'deg','rad');     % theta twist     [deg]
 
 
@@ -36,32 +37,32 @@ rotore = rotore.BEMT_articulated('T',W,V_inf,Chi,f,BEMTset_rotor());
 %% Graphics ---------------------------------------------------------------
 s = rotore.Analisi_articulated{1,1};
 figure;
-plot(s.mu,s.Pc_Vec,'-k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.Pc_Vec,'-k');
 hold on
 
 figure;
-plot(s.mu,s.Tc,'-k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.Tc,'-k');
 hold on
 
 figure;
-plot(s.mu,s.theta0*180/pi,'-k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.theta0*180/pi,'-k');
 hold on
 
 figure;
-plot(s.mu,s.alpha_TPP_Vec*180/pi,'-k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.alpha_TPP_Vec*180/pi,'-k');
 hold on
 
 figure;
-plot(s.mu,s.lam_Vec,'-k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.lam_Vec,'-k');
 hold on
-plot(s.mu,s.lam_i,':k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.lam_i,':k');
 plot(s.mu,s.lam_c,'.-k');
 
 figure;
-plot(s.mu,s.beta0_Vec*180/pi,'-k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.beta0_Vec*180/pi,'-k');
 hold on
-plot(s.mu,s.beta1c_Vec*180/pi,':k');
-plot(s.mu,s.beta1s_Vec*180/pi,'.-k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.beta1c_Vec*180/pi,':k');
+plot(s.mu.*cos(s.alpha_TPP_Vec).^-1,s.beta1s_Vec*180/pi,'.-k');
 
 
 
