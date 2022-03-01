249a0abdb9cd1b70b6aedb7b0c905406d1eee625
EmanueleSticchi <emanuele.sticchi@live.it>
2022-03-01 17:16:01 +0100

    correzioni e aggiunta di MAch map
    alpha map va bene ma i mach sono un po elevati

--- Black Hawk/Mappa_AoA.m
+++ Black Hawk/Mappa_AoA.m
@@ -5,22 +5,22 @@ global aero
 rotore   = Rotor();
 % aerodynamics
 load('polari HH_02\polari\Aero_HH02_Re1250.mat')
-rotore.Cl_alpha = aero.Cl_a_mode;
+rotore.Cl_alpha = aero.Cl_a_mode*180/pi;
 rotore.Cl       = @(alpha) CL_(alpha);
 rotore.Cd       = @(alpha) CD_(alpha);
 
 % working conditions
-rotore   = rotore.rot_vel('omega',convvel(726,'ft/s','m/s')/14.63);
+rotore   = rotore.rot_vel('omega',726/24);  % [rad/s]
 rotore.h = 0;                        % Assume density air = 1.23 Kg/m^3
 rotore   = rotore.ambient();         % compute ambient conditions
-V_inf    = linspace(0.1,convvel(200,'km/h','m/s'));
-Chi      = convang(0.001,'deg','rad');
+V_inf    = linspace(0.1,convvel(250,'km/h','m/s'));
+Chi      = convang(5,'deg','rad');
 f        = 2;
-W        = convforce(3000,'lbf','N');
+W        = convforce(17650,'lbf','N');
 
 % geometry
 rotore   = rotore.r(linspace(0.1,1,20));      % dominio radiale [\]
-rotore.R = 14.63;                             % Raggio rotore   [m]
+rotore.R = convlength(24,'ft','m');           % Raggio rotore   [m]
 rotore.N = 4;                                 % numero di pale  [\]
 rotore.c = linspace(0.53,0.53,rotore.n_r);    % corde           [m]
 I_MR     = 3800;
@@ -34,3 +34,6 @@ rotore.theta_t = convang(-9,'deg','rad');     % theta twist     [deg]
 
 alphamap(rotore,'Solve',{'T',W,V_inf([1:10:100]),Chi,f,BEMTset_rotor()});
 
+MachMap(rotore,'Solve',{'T',W,V_inf([1:10:100]),Chi,f,BEMTset_rotor()});
+% il numero di mach supera il valore sonico per velocità minore della V_ne.
+% In teoria bisognerebbe tenere in conto dell'effetto della freccia
