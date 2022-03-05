de5fc2633a6197508cc6fddae725948988b9ba0d
EmanueleSticchi <emanuele.sticchi@live.it>
2022-03-05 18:15:48 +0100

    alcune visualizzazioni di a e ap

--- Prove/BEMT_el_Hub.m
+++ Prove/BEMT_el_Hub.m
@@ -4,68 +4,84 @@ clc; clear; close all
 %%  DATI ----------------------------------------------------------
 global aero
 toll=1e-6;
-el=Elica();
+obj=Elica();
 
 % Geometrici
-el=el.r_((0.1:0.01:1)');
-el.N=3;                         % Numero di pale    
-el.D=2;                         % Diametro elica, [m]
-el.R=0.5*el.D;                  % Raggio dell'elica, [m]
-el.theta=pi/180*....            % Angolo di calettamento, [rad]
-    linspace(50,15,el.n_r)';
-el.c=0.1*ones(el.n_r,1);        % Corda delle sezioni, [m]
-el=el.sigma_();                 % Solidità
-
+obj=obj.r_((0.1:0.01:1)');
+obj.N=3;                         % Numero di pale    
+obj.D=2;                         % Diametro elica, [m]
+obj.R=0.5*obj.D;                  % Raggio dell'elica, [m]
+obj.theta=pi/180*....            % Angolo di calettamento, [rad]
+    linspace(50,15,obj.n_r)';
+obj.c=0.1*ones(obj.n_r,1);        % Corda delle sezioni, [m]
+obj=obj.sigma_();                 % Solidità
+obj.LAMBDA = zeros(obj.n_r,1);
 % Di funzionamento
-el=el.rot_vel('RPM',2000);
-el.rho=1.23;
-J=[0.:0.05:1.3];
+obj=obj.rot_vel('RPM',2000);
+obj= obj.altitude(0);
+J=[0.5:0.05:1.3];
 % Aerodinamici
 % Cl=@(alpha) interp1(data1,data2,alpha);
 % Cd=@(alpha) interp1(data1,data3,alpha);
 load Aero_NACA16212.mat
-el.Cl=@(alpha) CL_(alpha);
-el.Cd=@(alpha) CD_(alpha);
+obj.Cl=@(alpha,M,Re,r_bar) CL_(alpha);
+obj.Cd=@(alpha,M,Re,r_bar) CD_(alpha);
 
 %% Analisi BEMT --------------------------------------------------------
 alpha0=-2*pi/180;
 alpha1=10*pi/180;
 options=BEMTset();
-el=el.BEMT(J,alpha0,alpha1,options);
-options.Hub_correction='on';
-options.Cd_hub = 0.5;
-el=el.BEMT(J,alpha0,alpha1,options);
+obj=obj.BEMT(J,alpha0,alpha1,options);
+% options.Hub_correction='on';
+% options.Cd_hub = 0.5;
+% el=el.BEMT(J,alpha0,alpha1,options);
 
 %% Post - Processing
-an=2;
-for an=1:2
-figure(1)
-plotta(J,el.Analisi{an, 1}.CT,{'J = $ \frac{V_{\infty}}{nD}$';'$C_T$ = $\frac{T}{\rho n^2 D^4}$'})
-yline(0);
+s = obj.Analisi{1,1};
 
-figure(2)
-plotta(J,el.Analisi{an, 1}.CP,{'J = $ \frac{V_{\infty}}{nD}$';'$C_P$ = $\frac{P}{\rho n^3 D^5}$'})
-yline(0);
-
-figure(3)
-log=el.Analisi{an, 1}.CT>=0;
-plotta(J(log),el.Analisi{1, 1}.eta(log,1),{'J = $ \frac{V_{\infty}}{nD}$';'$\eta$ = $\frac{TV_{\infty}}{P}$'})
-yline(0);
-%
-idx=3;
-figure(4)
-plotta(el.r_bar,el.Analisi{an,1}.dCt_dr_bar(idx,:),{'$ \bar{r}$';'$\frac{dC_T}{d\bar{r}}$'})
-yline(0);
-figure(5)
-plotta(el.r_bar,el.Analisi{an,1}.dCq_dr_bar(idx,:),{'$ \bar{r}$';'$\frac{dC_Q}{d\bar{r}}$'})
-yline(0);
-figure(6)
-plotta(el.r_bar,el.Analisi{an,1}.dCp_dr_bar(idx,:),{'$ \bar{r}$';'$\frac{dC_P}{d\bar{r}}$'})
-yline(0);
-figure(7)
-plotta(el.r_bar,el.Analisi{an,1}.eta_e(idx,:),{'$ \bar{r}$';'$\eta_e$'})
-yline(0);
+for jdx = 1:length(J)
+   figure(1)
+   plot(obj.r_bar,s.a)
+   hold on
+   figure(2)
+   plot(obj.r_bar,s.ap)
+   hold on
 end
+
+
+
+
+
+% an=2;
+% for an=1:2
+% figure(1)
+% plotta(J,el.Analisi{an, 1}.CT,{'J = $ \frac{V_{\infty}}{nD}$';'$C_T$ = $\frac{T}{\rho n^2 D^4}$'})
+% yline(0);
+% 
+% figure(2)
+% plotta(J,el.Analisi{an, 1}.CP,{'J = $ \frac{V_{\infty}}{nD}$';'$C_P$ = $\frac{P}{\rho n^3 D^5}$'})
+% yline(0);
+% 
+% figure(3)
+% log=el.Analisi{an, 1}.CT>=0;
+% plotta(J(log),el.Analisi{1, 1}.eta(log,1),{'J = $ \frac{V_{\infty}}{nD}$';'$\eta$ = $\frac{TV_{\infty}}{P}$'})
+% yline(0);
+% %
+% idx=3;
+% figure(4)
+% plotta(el.r_bar,el.Analisi{an,1}.dCt_dr_bar(idx,:),{'$ \bar{r}$';'$\frac{dC_T}{d\bar{r}}$'})
+% yline(0);
+% figure(5)
+% plotta(el.r_bar,el.Analisi{an,1}.dCq_dr_bar(idx,:),{'$ \bar{r}$';'$\frac{dC_Q}{d\bar{r}}$'})
+% yline(0);
+% figure(6)
+% plotta(el.r_bar,el.Analisi{an,1}.dCp_dr_bar(idx,:),{'$ \bar{r}$';'$\frac{dC_P}{d\bar{r}}$'})
+% yline(0);
+% figure(7)
+% plotta(el.r_bar,el.Analisi{an,1}.eta_e(idx,:),{'$ \bar{r}$';'$\eta_e$'})
+% yline(0);
+% end
+
 
 %% Function ---------------------------------------------------------
 function CL=CL_(alpha)
