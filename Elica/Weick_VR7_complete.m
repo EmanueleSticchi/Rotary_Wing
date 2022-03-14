0a452d91f98afda043fe07def1302f6afa5be1f5
EmanueleSticchi <emanuele.sticchi@live.it>
2022-03-14 19:37:04 +0100

    some changes

--- Elica/Weick_VR7_complete.m
+++ Elica/Weick_VR7_complete.m
@@ -1,38 +1,20 @@
 %%  Analisi di un'elica Weick con la BEMT
+% modello aerodinamico completo profilo BV VR7
 clc; clear; close all
+flag = 0;
 global m_alpha m_Re m_Cl m_Cd
 load CC_experimential_Weick.mat
 %%  DATI ----------------------------------------------------------
-toll=1e-6;
-el=Elica();
-
-% Geometrici
-% interpolazione dei dati effettivi
-r    = convlength([10,12,18,24,30,36,42,48,54,60],'in','m');
-rb   = r/r(end);
-c    = convlength([6.97,...     % Corda delle sezioni, [m]
-    7.36,8.08,8.41,8.43,8.17,7.53,6.53,5.21,3.74],'in','m');
-theta = pi/180*....             % Angolo di calettamento, [rad]
-    [42.4,39.1,35.1,30.2,26.7,23.9,21.75,20.1,18.8,17.3];
-c_   = polyfit(rb,c,5);         % definizione di una polinomiale di 5*grado
-t_   = polyfit(rb,theta,5);     % definizione di una polinomiale di 5*grado
-
-el   = el.r_(rb(1):0.01:1);     % Definizione dominio radiale
-el.N = 3;                       % Numero di pale    
-el.R = convlength(63,'in','m'); % Raggio dell'elica, [m]
-el.c = polyval(c_,el.r_bar);    % Corda delle sezioni, [m]
-el   = el.derived_properties;   % compute some derived prop. like as sigma
-
-el.theta = polyval(t_,el.r_bar); % Angolo di calettamento, [rad]
-el.LAMBDA = zeros(el.n_r,1);    % Angolo di freccia, [rad]
+% importo l'oggeto elica
+load el_Weick
 
 % Di funzionamento
-theta_75 = convang(8.5,'deg','rad');
+theta_75 = convang(15.5,'deg','rad');
 [~,idx] = min(abs(el.r_bar - 0.75)); 
 el.theta = el.theta - el.theta(idx) + theta_75;  % setto il calettamento nominale al 75 %
-el=el.rot_vel('RPM',1000);   % valore medio di quelli nel paper
+el=el.rot_vel('RPM',1200);   % valore medio di quelli nel paper
 el=el.altitude(0);
-J=[0.05:0.05:1.0];
+J=[0.05:0.05:0.9];
 % Aerodinamici
 load AeroVR7_complete.mat
 el.Cl=@(alpha,r_bar,M,Re) Cl_(alpha,M,Re);
@@ -41,7 +23,9 @@ el.Cd=@(alpha,r_bar,M,Re) Cd_(alpha,M,Re);
 %% Analisi BEMT --------------------------------------------------------
 
 alpha0=-2*pi/180;
-options=BEMTset();      options.P_correction='on';
+options=BEMTset();              options.P_correction = 'on';
+options.Hub_correction = 'on';  options.Cd_hub       = 0.5;
+options.toll = 1e-4;
 el=el.BEMT(J,alpha0,options);
 
 %% Post - Processing
@@ -116,8 +100,16 @@ for i =4:15
     figure(i)
     lg = legend();
     lg.AutoUpdate='off';
+    lg.Location = 'best';
+    lg.Color    = 'none';
     yline(0);
     xline(0);
+end
+%% Salvataggio
+
+if flag ==1
+    save(1:3 ,'car','immagini/Weick_complete/caratteristiche/');
+    save(4:15,'distr','immagini/Weick_complete/distribuzioni/');
 end
 
 %% Plot comparison with experimental data
@@ -129,7 +121,7 @@ plotta(J,s.CT,...
 plotta(CC_exp.J,CC_exp.Ct,...
     {'J = $ \frac{V_{\infty}}{nD}$';'$C_T$ = $\frac{T}{\rho n^2 D^4}$'},...
     '--k',n_exp)
-yline(0);
+
 
 figure
 plotta(J,s.CP,...
@@ -138,9 +130,9 @@ plotta(J,s.CP,...
 plotta(CC_exp.J,CC_exp.Cp,...
     {'J = $ \frac{V_{\infty}}{nD}$';'$C_P$ = $\frac{P}{\rho n^3 D^5}$'},...
     '--k',n_exp)
-yline(0);
 
-figure(3)
+
+figure
 log=s.CT>=0;
 plotta(J(log),s.eta(log,1),...
     {'J = $ \frac{V_{\infty}}{nD}$';'$\eta$ = $\frac{TV_{\infty}}{P}$'},...
@@ -148,11 +140,19 @@ plotta(J(log),s.eta(log,1),...
 plotta(CC_exp.J,CC_exp.eta,...
     {'J = $ \frac{V_{\infty}}{nD}$';'$\eta$ = $\frac{TV_{\infty}}{P}$'},...
     '--k',n_exp)
-yline(0);
 
+for i =16:18
+    figure(i)
+    lg = legend();
+    lg.AutoUpdate='off';
+    lg.Color = 'none';
+    lg.Location = 'best';
+    yline(0);
+    xline(0);
+end
 
 if flag ==1
-    save(15:17 ,'car_comp','immagini/Weick_2pi/caratteristiche/');
+    save(16:18 ,'car_comp','immagini/Weick_complete/caratteristiche/');
 end
 
 % % MODEL 3D
@@ -167,7 +167,6 @@ end
 % plotta(el.r_bar,el.theta*180/pi,{'$ \bar{r}$';'$\theta$ [deg]'});
 
 %% Function ---------------------------------------------------------
-%% Function
 
 function Cl = Cl_(alpha,M,Re)
 % angoli in radianti
@@ -202,4 +201,18 @@ function Cd = Cd_(alpha,M,Re)
     else
         Cd = NaN;
     end
+end
+
+
+
+function save(idxF,prename,folder)
+count = 0;
+for i =1:length(idxF)
+    count = count + 1;
+    figure(idxF(i))
+    %     legend
+    FileName = sprintf([prename,'%d.eps'], count);
+    ax = gca;
+    exportgraphics(ax,[folder,FileName])
+end
 end
--- Elica/Weick_theta_VR7_complete.m
+++ Elica/Weick_theta_VR7_complete.m
@@ -1,24 +1,31 @@
 %%  Analisi di un'elica Weick con la BEMT al varia del passo di radice
+% modello aerodinamico completo profilo BV VR7
 clc; clear; close all
 flag = 1;
+global m_alpha m_Re m_Cl m_Cd
+
 %%  DATI ----------------------------------------------------------
 % importo l'oggeto elica
 load el_Weick
+% Aerodinamici
+load AeroVR7_complete.mat
+el.Cl=@(alpha,r_bar,M,Re) Cl_(alpha,M,Re);
+el.Cd=@(alpha,r_bar,M,Re) Cd_(alpha,M,Re);
 
 % Di funzionamento
-vtheta = convang(linspace(10,50,5),'deg','rad');
+vtheta = convang(linspace(10,30,5),'deg','rad');
 el=el.rot_vel('RPM',1200);   % valore medio di quelli nel paper
 el=el.altitude(0);
 options=BEMTset();      options.P_correction='on'; 
 options.Hub_correction = 'on'; options.Cd_hub =0.5;
 % alpha0 = [-2 -10 -10 -30 -50]*pi/180;
 alpha0 = -2*pi/180;
-J_end  = [1.3 1.4 1.6 2 2.5];
+J_end  = [0.6 0.75 1.25 1.35 1.5];
 for tdx = 1:length(vtheta)
     theta_75 = vtheta(tdx);
     [~,idx] = min(abs(el.r_bar - 0.75));
     el.theta = el.theta - el.theta(idx) + theta_75;  % setto il calettamento nominale al 75 %
-    J=[0.1:0.05:J_end(tdx)];
+    J=[0.05:0.05:J_end(tdx)];
     %% Analisi BEMT --------------------------------------------------------       
     el=el.BEMT(J,alpha0,options);
     
@@ -27,22 +34,21 @@ end
 %% Post - Processing    
 formatspec = {'-k';'--k';':k';'.-k';'^-k';'s-k'};
 for tdx = 1:length(vtheta)
-    J=[0.1:0.05:J_end(tdx)];
     s = el.Analisi{tdx, 1};
     name = ['\theta_{75} = ',num2str(vtheta(tdx)*180/pi),'°'];
     figure(1)
-    plotta(J,s.CT,...
+    plotta(s.J,s.CT,...
         {'J = $ \frac{V_{\infty}}{nD}$';'$C_T$ = $\frac{T}{\rho n^2 D^4}$'},...
         formatspec{tdx,1},name)
     
     figure(2)
-    plotta(J,s.CP,...
+    plotta(s.J,s.CP,...
         {'J = $ \frac{V_{\infty}}{nD}$';'$C_P$ = $\frac{P}{\rho n^3 D^5}$'},...
         formatspec{tdx,1},name)
     
     figure(3)
     log=s.CT>=0;
-    plotta(J(log),s.eta(log,1),...
+    plotta(s.J(log),s.eta(log,1),...
         {'J = $ \frac{V_{\infty}}{nD}$';'$\eta$ = $\frac{TV_{\infty}}{P}$'},...
         formatspec{tdx,1},name)    
 end
@@ -58,11 +64,48 @@ for i =1:3
 end
 
 if flag ==1
-    save(1:3,'car_theta','immagini/Weick_2pi/caratteristiche/')
+    save(1:3,'car_theta','immagini/Weick_complete/caratteristiche/')
 end
 
 
-%% Function 
+%% Function ---------------------------------------------------------
+
+function Cl = Cl_(alpha,M,Re)
+% angoli in radianti
+    global m_alpha m_Re m_Cl
+    alpha     = convang(alpha,'rad','deg');
+    alpha_lim = [m_alpha(1,1),m_alpha(1,end)];
+    Re_lim    = [m_Re(1,1)   ,m_Re(end,1)];
+    
+    Cl = interp2(m_alpha,m_Re,m_Cl,...
+        min(max(alpha_lim(1),alpha),alpha_lim(2)),...
+        min(max(Re_lim(1),Re),Re_lim(2)));
+    if M <1
+        Cl = Cl.*sqrt(1-M.^2).^-1;
+    else
+        Cl = NaN;
+    end
+end
+
+function Cd = Cd_(alpha,M,Re)
+% angoli in radianti
+    global m_alpha m_Re m_Cd
+    alpha     = convang(alpha,'rad','deg');
+    alpha_lim = [m_alpha(1,1),m_alpha(1,end)];
+    Re_lim    = [m_Re(1,1)   ,m_Re(end,1)];
+    
+    Cd = interp2(m_alpha,m_Re,m_Cd,...
+        min(max(alpha_lim(1),alpha),alpha_lim(2)),...
+        min(max(Re_lim(1),Re),Re_lim(2)));
+    if M <1
+        k  = 6; k2 = 0.7;
+        Cd = (Cd-k2) + k2*(1+0.25*M^k);
+    else
+        Cd = NaN;
+    end
+end
+
+
 
 function save(idxF,prename,folder)
 count = 0;
